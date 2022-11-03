badfe1a55d9714115a19889176b5ebbe9e6624c2
hellojoyworldz <hellojoyworldz@gmail.com>
2022-11-03 16:23:19 +0900

    commit

---
+++ php2/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 사이트 만들기</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="html/assets/css/style.css">
+    <!-- META -->
+    <meta name="author" content="webstoryboy">
+    <meta name="description" content="PHP 사이트 만들기입니다.">
+    <meta name="keyword" content="사이트, 만들기, 튜토리얼, 웹스토리보이">
+    <meta name="robots" content="all">
+    <!-- ICON -->
+    <link rel="icon" href="assets/img/icon_256.png" />
+    <link rel="shortcut icon" href="assets/img/icon_256.png" />
+    <link rel="icon" type="image/png" sizes="256x256" href="assets/img/icon_256.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/icon_192.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/icon_32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/icon_16.png" />
+    <style>
+    body {
+        padding: 20px;
+    }
+
+    .list {
+        margin: 20px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="list">
+        <ul>
+            <li>PHP myAdmin : <a href="http://clickb1270.dothome.co.kr/myadmin/">MyAdmin</a></li>
+            <li>데이터베이스 접속 : <a href="connect/connect.php">connect.php</a></li>
+
+            <li>회원가입 테이블 만들기 : <a href="create/createMember.php">createMember.php</a></li>
+            <li>게시판 테이블 만들기 : <a href="create/createBoard.php">createBoard.php</a></li>
+            <li>게시판 데이터 : <a href="create/createBoardData.php">createBoardData.php</a></li>
+            <li>블로그 테이블 만들기 : <a href="create/createBlog.php">createBlog.php</a></li>
+            <li>댓글 테이블 만들기 : <a href="create/createComment.php">createComment.php</a></li>
+        </ul>
+        <br /><br />
+        <ul>
+            <li>회원가입 페이지 : <a href="login/join.php">join.php</a></li>
+            <li>회원가입 데이터 저장 페이지 : <a href="login/joinSave.php">joinSave.php</a></li>
+            <li>로그인 페이지 : <a href="login/login.php">login.php</a></li>
+            <li>로그인 데이터 저장 페이지 : <a href="login/loginSave.php">loginSave.php</a></li>
+            <li>로그아웃 : <a href="login/logout.php">logout.php</a></li>
+        </ul>
+        <br /><br />
+        <ul>
+            <li>게시판 페이지 : <a href="board/board.php">board.php</a></li>
+            <li>게시판 쓰기 : <a href="board/boardWrite.php">boardWrite.php</a></li>
+            <li>게시판 보기 : <a href="board/boardView.php">boardView.php</a></li>
+            <li>게시판 삭제 : <a href="board/boardModify.php">boardModify.php</a></li>
+        </ul>
+        <br /><br />
+        <ul>
+            <li>블로그 페이지 : <a href="blog/blog.php">blog.php</a></li>
+            <li>블로그 카테고리 : <a href="blog/blogCategory.php">blogCategory.php</a></li>
+            <li>블로그 검색 : <a href="blog/blogSearch.php">blogSearch.php</a></li>
+            <li>블로그 쓰기 : <a href="blog/blogWrite.php">blogWrite.php</a></li>
+            <li>블로그 수정 : <a href="blog/blogModify.php">blogModify.php</a></li>
+            <li>블로그 댓글 쓰기 : <a href="blog/blogCommentWrite.php">blogCommentWrite.php</a></li>
+            <li>블로그 댓글 수정 : <a href="blog/blogCommentModify.php">blogCommentModify.php</a></li>
+            <li>블로그 댓글 삭제 : <a href="blog/blogCommentDelete.php">blogCommentDelete.php</a></li>
+        </ul>
+
+    </div>
+</body>
+
+</html>
